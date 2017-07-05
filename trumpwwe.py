084e97f718d08dc4fdcfdbde6cfb5795e44c0742
Varun Ramachandran <varun.rama@gmail.com>
2017-07-05 09:10:16 -0400

    got rid of ur trash

--- trumpwwe.py
+++ trumpwwe.py
@@ -1,11 +1,18 @@
 import cv2
 import sys
+import imageio
+import os
+
+import string
+import random
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+#time to clean this up for server use
 
 if __name__ == '__main__':
-    # i stole like more than half this shit
-    # Set up tracker.
-    
-	# Instead of MIL, you can also use
+
+    # alternative algorithms
     # BOOSTING, KCF, TLD, MEDIANFLOW or GOTURN
     #MEDIANFLOW seems to be the fastest and most accurate for occlusions of dude's face
     tracker = cv2.Tracker_create("MEDIANFLOW")
@@ -22,27 +29,25 @@ if __name__ == '__main__':
     if not ok:
         print('Cannot read video file')
         sys.exit()
+
     my_img = cv2.imread("yashface.png")
 
-    # Define an initial bounding box
+    # what it starts tracking, from the first frame
+    #dude's face
     bbox = (180, 83, 60, 60)
 
-    # Uncomment the line below to select a different bounding box
-    # bbox = cv2.selectROI(frame, False)
 
     # Initialize tracker with first frame and bounding box
     ok = tracker.init(frame, bbox)
-
+    img_list = []
     while True:
         # Read a new frame
         ok, frame = video.read()
         if not ok:
             break
-
         # Update tracker
         ok, bbox = tracker.update(frame)
-
-        # Draw bounding box
+        # tracker there
         if ok:
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
@@ -52,12 +57,16 @@ if __name__ == '__main__':
             y_coord = int(bbox[1])
             x_coord = int(bbox[0])
             frame[y_coord:y_coord+resized_image.shape[0], x_coord: x_coord + resized_image.shape[1]] = resized_image
+            img_list.append(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
 
-            # cv2.rectangle(frame, p1, p2, (0, 0, 255))
+    print("done processing gif")
+    #now we generate gif
 
-        # Display result
-        cv2.imshow("Tracking", frame)
+    kargs =  {"duration" : .02}
+    with imageio.get_writer('movie.gif', mode='I', **kargs) as writer:
+        # for filename in sorted(os.listdir("./frames"), key= lambda x : int(x.split(".")[0])):
+        #     image = imageio.imread("./frames/" + filename)
+        #     writer.append_data(image)
+        for image in img_list:
 
-        # Exit if ESC pressed
-        k = cv2.waitKey(1) & 0xff
-        if k == 27: break
+            writer.append_data(image)
