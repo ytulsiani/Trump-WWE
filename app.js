f7f4e3408e9642e4438611c1318e2a64782cbe20
Yash Tulsiani <ytulsiani3@gatech.edu>
2017-07-08 16:23:38 -0400

    fixed port issue

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var express = require('express')
 var app = express()
-var port = process.env.VCAP_APP_PORT || 3000;
+var port = process.env.PORT || 5000;
 var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
