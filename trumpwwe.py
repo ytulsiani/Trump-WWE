7ec49223b6a668e1b55906439c093b79216d40f2
Varun Ramachandran <varun.rama@gmail.com>
2017-07-10 14:51:03 -0400

    #integrationtesting

--- trumpwwe.py
+++ trumpwwe.py
@@ -6,69 +6,85 @@ import string
 import random
 
 #time to clean this up for server use
+def getTracker(frame, bbox):
+    tracker = cv2.Tracker_create("MEDIANFLOW")
+    tracker.init(frame, bbox)
+    return tracker
 
 def main():
     #args = get_args()
     id = sys.argv[1]
-
+	
     # alternative algorithms
     # BOOSTING, KCF, TLD, MEDIANFLOW or GOTURN
     #MEDIANFLOW seems to be the fastest and most accurate for occlusions of dude's face
     tracker = cv2.Tracker_create("MEDIANFLOW")
     # Read video
-    video = cv2.VideoCapture("vid1.avi")
+    # video = cv2.VideoCapture("vid1.mp4")
 
     # Exit if video not opened.
-    if not video.isOpened():
-        print("Could not open video")
-        sys.exit()
+    # if not video.isOpened():
+    #     print("Could not open video")
+    #     sys.exit()
 
     # Read first frame.
-    ok, frame = video.read()
-    if not ok:
-        print('Cannot read video file')
-        sys.exit()
+    # ok, frame = video.read()
+    # if not ok:
+    #     print('Cannot read video file')
+    #     sys.exit()
+    
     imageLoc = "./uploads/" + id + ".jpg"
     my_img = cv2.imread(imageLoc)
-    print('image upload')
+    # print('image upload')
 
     # what it starts tracking, from the first frame
     #dude's face
-    bbox = (180, 83, 60, 60)
+    frame = cv2.imread('./frames/frame_000.jpg')
+    
 
-
+    bbox = (175, 80, 60, 60)
     # Initialize tracker with first frame and bounding box
     ok = tracker.init(frame, bbox)
-    img_list = []
-    while True:
-        # Read a new frame
-        ok, frame = video.read()
-        if not ok:
-            break
-        # Update tracker
-        ok, bbox = tracker.update(frame)
-        # tracker there
-        if ok:
-            p1 = (int(bbox[0]), int(bbox[1]))
-            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            #put in my stuff (resize image then put it in ROI
-            assuranceSize = 40
-            resized_image = cv2.resize(my_img, (int(bbox[2]+ assuranceSize), int(bbox[3] + assuranceSize)))
-            y_coord = int(bbox[1])
-            x_coord = int(bbox[0])
-            frame[y_coord:y_coord+resized_image.shape[0], x_coord: x_coord + resized_image.shape[1]] = resized_image
-            img_list.append(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
 
-    print("done processing gif")
+    readj_points = {116: (284,176, 50, 50),  148: (285, 199, 40,40)}
+
+    kargs =  {"duration" : .08}
+    frame_count = 1
+    with imageio.get_writer('./public/gifs/'+id +'.gif', mode='I', **kargs) as writer:
+        for filename in sorted(os.listdir("./frames")):
+            frame = cv2.imread("./frames/" + filename)
+            
+            #readjust the tracker 
+            if frame_count in readj_points:
+                tracker = getTracker(frame, readj_points[frame_count])
+
+            #update tracker object
+            ok, bbox = tracker.update(frame)
+
+            # tracker there
+            if ok:
+                p1 = (int(bbox[0]), int(bbox[1]))
+                p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+                #put in my stuff (resize image then put it in ROI
+                assuranceSize = 40
+                resized_image = cv2.resize(my_img, (int(bbox[2]+ assuranceSize), int(bbox[3] + assuranceSize)))
+                y_coord = int(bbox[1])
+                x_coord = int(bbox[0])
+
+                frame[y_coord:y_coord+resized_image.shape[0], x_coord: x_coord + resized_image.shape[1]] = resized_image
+
+            #compress the image
+            frame = cv2.resize(frame, (320, 180))
+            #write it to the gif 
+            if (frame_count % 2 == 1):
+                writer.append_data(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+            
+
+
+            frame_count += 1
+    print(id)
     #now we generate gif
 
-    kargs =  {"duration" : .02}
-    with imageio.get_writer('./public/gifs/'+id +'.gif', mode='I', **kargs) as writer:
-        # for filename in sorted(os.listdir("./frames"), key= lambda x : int(x.split(".")[0])):
-        #     image = imageio.imread("./frames/" + filename)
-        #     writer.append_data(image)
-        for image in img_list:
 
-            writer.append_data(image)
 if __name__ == '__main__':
     main()
